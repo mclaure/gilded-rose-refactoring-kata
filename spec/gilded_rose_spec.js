172f7869a6236967c4ee29d27140b06eee7c5b5d
Marcelo Claure <marcelo.claure@niceincontact.com>
2019-10-15 17:41:35 -0400

    First Test

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,10 +1,13 @@
 var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
-
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+  describe("When 'Aged Brie' item gets older", function(){  
+    it("Should increase Quality in 2", function() {
+      var gildedRose = new Shop([ new Item("Aged Brie", 0, 0) ]);      
+      var items = gildedRose.updateQuality();
+      console.log(items);
+      console.log(items[0].quality);
+      expect(items[0].name).toEqual("Aged Brie");
+      expect(items[0].quality).toEqual(2);
+    });
   });
-
 });
